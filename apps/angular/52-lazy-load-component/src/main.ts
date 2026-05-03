19c5d985e5d4a5ae6e25e3f1a71265c2af425e21
ihabChaker <benakchaihab@gmail.com>
2026-05-03 18:45:31 +0200

    added top component lazy loading

--- apps/angular/52-lazy-load-component/src/main.ts
+++ apps/angular/52-lazy-load-component/src/main.ts
@@ -1,7 +1,6 @@
-import { provideZoneChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, {
-  providers: [provideZoneChangeDetection()],
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, { providers: [] }).catch((err) =>
+  console.error(err),
+);
