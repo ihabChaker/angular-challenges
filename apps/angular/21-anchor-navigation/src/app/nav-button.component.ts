f85d5b9d040aec460e6a3f2e83d32840b9a06359
ihabChaker <benakchaihab@gmail.com>
2026-05-03 14:33:54 +0200

    feat(): enhance anchor navigation with smooth scrolling and fragment support

--- apps/angular/21-anchor-navigation/src/app/nav-button.component.ts
+++ apps/angular/21-anchor-navigation/src/app/nav-button.component.ts
@@ -1,17 +1,19 @@
 /* eslint-disable @angular-eslint/component-selector */
 import { Component, input } from '@angular/core';
-
+import { RouterLinkWithHref } from '@angular/router';
 @Component({
   selector: 'nav-button',
   template: `
-    <a [href]="href()">
+    <a [routerLink]="href()" [fragment]="fragment()">
       <ng-content />
     </a>
   `,
+  imports: [RouterLinkWithHref],
   host: {
     class: 'block w-fit border border-red-500 rounded-md p-4 m-2',
   },
 })
 export class NavButtonComponent {
   href = input('');
+  fragment = input<string | undefined>(undefined);
 }
