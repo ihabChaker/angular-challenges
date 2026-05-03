ddf80b577a1e200fe98f8fbe395ea0efd7ff3dc9
ihabChaker <benakchaihab@gmail.com>
2026-05-03 18:58:13 +0200

    feat(): implement content projection with app-title and app-message components

--- apps/angular/57-content-projection-default/src/app/app.component.ts
+++ apps/angular/57-content-projection-default/src/app/app.component.ts
@@ -2,11 +2,30 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CardComponent } from './card.component';
 
 @Component({
-  imports: [CardComponent],
+  selector: 'app-title',
+  template: `
+    <div><ng-content /></div>
+  `,
+})
+class AppTitle {}
+
+@Component({
+  selector: 'app-message',
+  template: `
+    <div><ng-content /></div>
+  `,
+})
+class AppMessage {}
+
+@Component({
+  imports: [CardComponent, AppTitle, AppMessage, AppTitle],
   selector: 'app-root',
   template: `
-    <app-card title="Titre 1" message="Message1" />
-    <app-card title="Titre 2" />
+    <app-card>
+      <app-title>Titre 1</app-title>
+      <app-message>Message1</app-message>
+    </app-card>
+    <app-card><app-title>Titre 2</app-title></app-card>
   `,
   host: {
     class: 'p-4 block flex flex-col gap-1',
