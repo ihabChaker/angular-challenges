80a344e4c55e0206a35f489ad02043a0423303bf
ihabChaker <benakchaihab@gmail.com>
2026-05-16 19:39:49 +0200

    test(): implement cypress test suite

--- apps/testing/17-router/src/app/app.component.cy.ts
+++ apps/testing/17-router/src/app/app.component.cy.ts
@@ -1,33 +1,69 @@
+import { provideRouter } from '@angular/router';
 import { AppComponent } from './app.component';
+import { appRoutes } from './app.routes';
 
 describe(AppComponent.name, () => {
+  function setup() {
+    cy.mount(AppComponent, {
+      providers: [provideRouter(appRoutes)],
+    });
+    // Necessary for directing the process to the search page
+    cy.get('a').click();
+  }
+
   describe('Given no search criteria', () => {
     it('Then shows error message and disabled button', () => {
-      //todo
+      setup();
+
+      cy.get('.error').should('have.text', 'Search criteria is required!');
+      cy.get('[data-cy="borrow-btn"]').should('have.attr', 'disabled');
     });
   });
 
   describe('Given a search criteria with no book match', () => {
     it('Then shows No book found', () => {
-      //todo
+      setup();
+
+      cy.get('#bookName').type('glfdkglfdkl');
+      cy.get('[data-cy="borrow-btn"]').click();
+
+      cy.get('ng-component').should(
+        'have.text',
+        'No book found for this search',
+      );
     });
   });
 
   describe('Given a search criteria with one book match', () => {
     it('Then shows One book and no error', () => {
-      //todo
+      setup();
+
+      cy.get('#bookName').type('kill');
+      cy.get('[data-cy="borrow-btn"]').click();
+
+      cy.get('li').should('have.length', 1);
     });
   });
 
   describe('Given a search criteria in Uppercase with one book match', () => {
     it('Then shows One book and no error', () => {
-      //todo
+      setup();
+
+      cy.get('#bookName').type('kill'.toUpperCase());
+      cy.get('[data-cy="borrow-btn"]').click();
+
+      cy.get('li').should('have.length', 1);
     });
   });
 
   describe('Given a search criteria with multple books matches', () => {
     it('Then shows a list of books', () => {
-      //todo
+      setup();
+
+      cy.get('#bookName').type('The'.toUpperCase());
+      cy.get('[data-cy="borrow-btn"]').click();
+
+      cy.get('li').should('have.length.greaterThan', 1);
     });
   });
 });
