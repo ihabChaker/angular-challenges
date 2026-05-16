ff2570dd496ec0c260bf102270d6200b1b093695
ihabChaker <benakchaihab@gmail.com>
2026-05-16 23:52:07 +0200

    feat(): enhance ChildComponent tests and update tsconfig for better type inclusion

--- apps/testing/18-nested-components/src/app/child.component.cy.ts
+++ apps/testing/18-nested-components/src/app/child.component.cy.ts
@@ -1,18 +1,45 @@
 import { ChildComponent } from './child.component';
+import { HttpService } from './http.service';
 
 describe('ChildComponent', () => {
+  let httpService = new HttpService();
+  let spy: Cypress.Agent<sinon.SinonSpy>;
+
   const setup = () => {
-    cy.mount(ChildComponent);
+    spy = cy.spy(httpService, 'sendTitle');
+
+    cy.mount(ChildComponent, {
+      providers: [
+        {
+          provide: HttpService,
+          useValue: httpService,
+        },
+      ],
+    });
   };
+
+  beforeEach(() => {
+    setup();
+  });
   describe('When typing nothing and clicking on Validate', () => {
     test('Then show "Title is required" error message and no http request has been sent', async () => {
-      setup();
+      cy.get('input').clear();
+      cy.get('button').click();
+
+      cy.contains('Title is required');
+      cy.wrap(spy).should('not.be.called');
     });
   });
 
   describe('When typing "Good" and clicking on Validate', () => {
     test('Then show "Title is Good" message, no error message and send a http request to the backend', async () => {
-      setup();
+      let title = 'Good';
+      cy.get('input').clear().type(title);
+      cy.get('button').click();
+
+      cy.contains('Title is ' + title);
+      cy.wrap(spy).should('be.calledWith', title);
+      // expect(spy).to.be.calledWith(title);
     });
   });
 });
--- apps/testing/18-nested-components/src/app/child.component.spec.ts
+++ apps/testing/18-nested-components/src/app/child.component.spec.ts
@@ -1,16 +1,55 @@
-import { render } from '@testing-library/angular';
+import { render, screen } from '@testing-library/angular';
+import userEvent from '@testing-library/user-event';
+
 import { ChildComponent } from './child.component';
+import { HttpService } from './http.service';
 
 describe('ChildComponent', () => {
+  let httpServiceMock = {
+    sendTitle: jest.fn(),
+  };
+
+  beforeEach(async () => {
+    const component = await render(ChildComponent, {
+      providers: [
+        {
+          provide: HttpService,
+          useValue: httpServiceMock,
+        },
+      ],
+    });
+    component.detectChanges();
+  });
   describe('When typing nothing and clicking on Validate', () => {
     test('Then show "Title is required" error message and no http request has been sent', async () => {
-      await render(ChildComponent);
+      const user = userEvent.setup();
+
+      let input = screen.getByRole('textbox');
+      await user.clear(input);
+
+      let btn = screen.getByRole('button', { name: /validate/i });
+      await user.click(btn);
+
+      expect(screen.getByText(/Title is required !!!/i)).toBeInTheDocument();
+      expect(httpServiceMock.sendTitle).not.toHaveBeenCalled();
     });
   });
 
   describe('When typing "Good" and clicking on Validate', () => {
     test('Then show "Title is Good" message, no error message and send a http request to the backend', async () => {
-      await render(ChildComponent);
+      const user = userEvent.setup();
+
+      let input = screen.getByRole('textbox');
+      await user.clear(input);
+
+      const title = 'Good';
+      await user.type(input, title);
+
+      let btn = screen.getByRole('button', { name: /validate/i });
+      await user.click(btn);
+
+      expect(screen.getByText(/Title is Good/i)).toBeInTheDocument();
+      expect(httpServiceMock.sendTitle).toHaveBeenCalledWith(title);
     });
   });
 });
