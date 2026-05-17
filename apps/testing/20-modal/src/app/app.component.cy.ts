51d852eae06705bcfa9616573dca8a4890759773
ihabChaker <benakchaihab@gmail.com>
2026-05-17 12:45:06 +0200

    test(): implemented test suites

--- apps/testing/20-modal/src/app/app.component.cy.ts
+++ apps/testing/20-modal/src/app/app.component.cy.ts
@@ -4,16 +4,31 @@ describe(AppComponent.name, () => {
   const setup = () => {
     cy.mount(AppComponent);
   };
-
-  test('error modal is displayed if you click on "Confirm" without inputing a name', () => {
+  beforeEach(() => {
     setup();
+  });
+  test('error modal is displayed if you click on "Confirm" without inputing a name', () => {
+    cy.get('input').clear();
+    cy.contains('button', 'Confirm').click();
+
+    cy.contains(/error/i).should('be.visible');
   });
 
   test('error message is shown if you click "Cancel" in the confirmation modal after submitting a name', () => {
-    setup();
+    cy.get('input').clear().type('foobar');
+    cy.contains('button', 'Confirm').click();
+    cy.contains('button', 'Cancel').click();
+
+    cy.contains(/Name is invalid !!/i).should('be.visible');
   });
 
   test('confirm message is shown if you click "Confirm" in the confirmation modal after submitting a name', () => {
-    setup();
+    let name = 'foobar';
+    cy.get('input').clear().type(name);
+    cy.contains('button', 'Confirm').click();
+    cy.contains('button', 'Confirmation').click();
+
+    cy.contains(/Profil/i).should('be.visible');
+    cy.contains(`Name: ${name}`).should('be.visible');
   });
 });
--- apps/testing/20-modal/src/app/app.component.spec.ts
+++ apps/testing/20-modal/src/app/app.component.spec.ts
@@ -1,16 +1,70 @@
-import { render } from '@testing-library/angular';
+import { render, screen } from '@testing-library/angular';
+import userEvent from '@testing-library/user-event';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  test('error modal is displayed if you click on "Confirm" without inputing a name', async () => {
+  beforeEach(async () => {
     await render(AppComponent);
+  });
+  test('error modal is displayed if you click on "Confirm" without inputing a name', async () => {
+    let user = userEvent.setup();
+
+    let input = screen.getByRole('textbox', {
+      name: /name/i,
+    });
+    await user.clear(input);
+
+    let submitBtn = screen.getByRole('button', {
+      name: /confirm/i,
+    });
+    await user.click(submitBtn);
+
+    expect(await screen.findByText(/error/i)).toBeInTheDocument();
   });
 
   test('error message is shown if you click "Cancel" in the confirmation modal after submitting a name', async () => {
-    await render(AppComponent);
+    let user = userEvent.setup();
+
+    let input = screen.getByRole('textbox', {
+      name: /name/i,
+    });
+    await user.clear(input);
+    await user.type(input, 'foobar');
+
+    let submitBtn = screen.getByRole('button', {
+      name: /confirm/i,
+    });
+    await user.click(submitBtn);
+
+    let cancelBtn = screen.getByRole('button', {
+      name: /cancel/i,
+    });
+    await user.click(cancelBtn);
+
+    expect(await screen.findByText(/Name is invalid !!/i)).toBeInTheDocument();
   });
 
   test('confirm message is shown if you click "Confirm" in the confirmation modal after submitting a name', async () => {
-    await render(AppComponent);
+    let user = userEvent.setup();
+
+    let input = screen.getByRole('textbox', {
+      name: /name/i,
+    });
+    await user.clear(input);
+    await user.type(input, 'foobar');
+
+    let submitBtn = screen.getByRole('button', {
+      name: /confirm/i,
+    });
+    await user.click(submitBtn);
+
+    let confirmationBtn = screen.getByRole('button', {
+      name: /Confirmation/i,
+    });
+    await user.click(confirmationBtn);
+
+    expect(
+      await screen.findByText(/Name has been submitted/i),
+    ).toBeInTheDocument();
   });
 });
