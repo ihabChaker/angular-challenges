29e933875293cfbd052e2c43eff5ab0df22e8b5d
ihabChaker <benakchaihab@gmail.com>
2026-06-03 21:27:20 +0200

    refactor(): update UserComponent to use input() for properties

--- apps/signal/43-signal-input/src/app/user.component.ts
+++ apps/signal/43-signal-input/src/app/user.component.ts
@@ -2,7 +2,8 @@ import { TitleCasePipe } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
-  Input,
+  input,
+  numberAttribute,
   OnChanges,
 } from '@angular/core';
 
@@ -26,15 +27,15 @@ const ageToCategory = (age: number): Category => {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserComponent implements OnChanges {
-  @Input({ required: true }) name!: string;
-  @Input() lastName?: string;
-  @Input() age?: string;
+  name = input.required<string>();
+  lastName = input<string>();
+  age = input(0, { transform: numberAttribute });
 
   fullName = '';
   category: Category = 'Junior';
 
   ngOnChanges(): void {
-    this.fullName = `${this.name} ${this.lastName ?? ''}`;
-    this.category = ageToCategory(Number(this.age));
+    this.fullName = `${this.name()} ${this.lastName() ?? ''}`;
+    this.category = ageToCategory(this.age());
   }
 }
